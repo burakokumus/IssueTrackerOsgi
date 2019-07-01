1ed6113c0ae2d2d3f2ffb2cd96af7b432df1d375
Burak <burakokumus98@gmail.com>
2019-07-01 17:47:03 +0300

    initial commit

--- UserInterface/src/com/ekinoks/view/LoginView.java
+++ UserInterface/src/com/ekinoks/view/LoginView.java
@@ -10,7 +10,7 @@ import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
-
+//sdfsf
 @SuppressWarnings("serial")
 public class LoginView extends JDialog
 {
